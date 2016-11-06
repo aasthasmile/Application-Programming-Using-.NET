30f37060f624c97fe8bffd56a0cb44b1399bf47a
aasthasmile <aasthasmile.jain@gmail.com>
2016-11-06 00:40:33 -0700

    Web Service REST using XML based implementation
    REST services and Client Application running
    on http://localhost:52163//PhoneBookRESTXMLService.svc. It has 2
    operations Add Entry and Get Entries using LastName

---
+++ Assignment9_RESTXML/Assignment9_RESTXML/PhoneBookRESTXMLService/PhoneBookRESTXMLService/IPhoneBookRESTXMLService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PhoneBookRESTXMLService
+{
+   [ServiceContract]
+   public interface IPhoneBookRESTXMLService
+   {
+       // add an entry to the phone book database
+       [OperationContract]
+       [WebGet(UriTemplate = "/AddEntry/{LastName}/{FirstName}/{PhoneNumber}")]
+        void AddEntry(string LastName, string FirstName, string PhoneNumber);
+
+        // retrieve phone book entries with a given last name
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetEntry/{LastName}")]
+        IQueryable<PhoneBook> GetEntry(string LastName);
+
+   } // end interface IPhoneBookRESTXMLService
+}
+
---
+++ Assignment9_RESTXML/Assignment9_RESTXML/PhoneBookRESTXMLService/PhoneBookRESTXMLService/PhoneBookRESTXMLService.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PhoneBookRESTXMLService
+{
+   public class PhoneBookRESTXMLService : IPhoneBookRESTXMLService
+   {
+      // create a dbcontext object to access PhoneBook database
+      private PhoneBookEntities dbcontext = new PhoneBookEntities();
+
+      // add an entry to the phone book database
+      public void AddEntry(string lastName, string firstName,
+         string phoneNumber)
+      {
+            // create PhoneBook entry to be inserted in database
+            PhoneBook newEntry = new PhoneBook()
+            {
+                LastName = lastName,
+                FirstName = firstName,
+                PhoneNumber = phoneNumber
+
+            };
+            // insert PhoneBook entry in database
+            dbcontext.PhoneBooks.Add(newEntry);
+            dbcontext.SaveChanges();
+        } // end method AddEntry
+
+        // retrieve phone book entries with a given last name
+        public IQueryable<PhoneBook> GetEntry(string LastName)
+        {
+            //var query =( from phone in dbcontext.PhoneBooks
+            //            where phone.LastName == LastName
+            //            select phone).;
+           
+            return dbcontext.PhoneBooks.Where(d=>d.LastName == LastName);
+        }
+    }
+}
+
